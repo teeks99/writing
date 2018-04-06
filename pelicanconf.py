086aa5a71415d9f75094e8b4de3f0a4465072523
Thomas Kent <tom@teeks99.com>
2018-04-06 16:11:23 -0500

    Latest versions

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,6 +5,7 @@ from __future__ import unicode_literals
 AUTHOR = 'Tom'
 SITENAME = 'Tom\'s Writing'
 SITEURL = ''
+#THEME = "myidea"
 
 PATH = 'content'
 
@@ -19,6 +20,12 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+#PAGE_PATH = ['pages']
+#DISPLAY_PAGES_ON_MENU = False
+#MENUITEMS = (
+#    ('Homepage', 'https://teeks99.com')
+#)
+
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
@@ -30,6 +37,7 @@ SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = False
+#INDEX_SAVE_AS = 'posts_index.html'
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
